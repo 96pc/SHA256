aa459a08a4f83461148ddd985ac7b07753fa0417
96pc <pangcheng1996@outlook.com>
2023-12-01 00:52:43 +0800

    2023-12-01 00:52:43

--- main.py
+++ main.py
@@ -58,3 +58,4 @@ bool WtSimpDataMgr::initStore(WTSVariant* cfg)
 	else
 		module = WtHelper::getInstDir() + DLLHelper::wrap_module(module.c_str());
 
+	DllHandle hInst = DLLHelper::load_library(module.c_str());
