2156e829966316a876ff3b21633b2c778feb525c
96pc <pangcheng1996@outlook.com>
2023-12-08 23:10:24 +0800

    2023-12-08 23:10:24

--- main.py
+++ main.py
@@ -67,3 +67,4 @@ bool WtSimpDataMgr::initStore(WTSVariant* cfg)
 
 	FuncCreateDataReader funcCreator = (FuncCreateDataReader)DLLHelper::get_symbol(hInst, "createDataReader");
 	if (funcCreator == NULL)
+	{
