5c3471c53329b78d20dac5283962d70eadafe195
96pc <pangcheng1996@outlook.com>
2024-11-30 14:47:48 +0800

    2024-11-30 14:47:48

--- main.py
+++ main.py
@@ -331,3 +331,4 @@ bool WtSimpDataMgr::initStore(WTSVariant* cfg)
 	if (module.empty())
 		module = WtHelper::getInstDir() + DLLHelper::wrap_module("WtDataStorage");
 	else
+		module = WtHelper::getInstDir() + DLLHelper::wrap_module(module.c_str());
