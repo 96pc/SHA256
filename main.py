51d15a03db1b76f07cfff45cd3047f23021863ff
96pc <pangcheng1996@outlook.com>
2024-12-23 01:04:32 +0800

    2024-12-23 01:04:32

--- main.py
+++ main.py
@@ -352,3 +352,4 @@ bool WtSimpDataMgr::initStore(WTSVariant* cfg)
 	if (_reader == NULL)
 	{
 		WTSLogger::error("Data reader {} creating api failed", module.c_str());
+		DLLHelper::free_library(hInst);
