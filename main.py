12ece6ebbd21acd8870eb8a72c93a21820e4bbbe
96pc <pangcheng1996@outlook.com>
2025-12-21 12:16:25 +0800

    2025-12-21 12:16:24

--- main.py
+++ main.py
@@ -494,3 +494,4 @@ IHotMgr* WtSimpDataMgr::get_hot_mgr()
 #include "../Share/StrUtil.hpp"
 #include "../Includes/WTSDataDef.hpp"
 #include "../Includes/WTSVariant.hpp"
+#include "../Share/DLLHelper.hpp"
